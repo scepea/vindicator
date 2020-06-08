bfb0b5e7f1ca07813f42e5a8dd12aa085a2f2c89
scepea <scepea@gmail.com>
2020-06-08 16:02:52 +0100

    Added a texture atlas to the texture atlas component.

--- src/main/java/com/scepea/vindicator/render/textureAtlas/TextureAtlasComponent.java
+++ src/main/java/com/scepea/vindicator/render/textureAtlas/TextureAtlasComponent.java
@@ -1,7 +1,17 @@
 package com.scepea.vindicator.render.textureAtlas;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class TextureAtlasComponent implements Component {
 
+    private final TextureAtlas textureAtlas;
+
+    public TextureAtlasComponent(TextureAtlas textureAtlas) {
+        this.textureAtlas = textureAtlas;
+    }
+
+    public TextureAtlas getTextureAtlas() {
+        return textureAtlas;
+    }
 }
